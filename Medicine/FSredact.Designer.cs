0717250290c142da062083006f118da41a5e6cd9
MrHantLP <mrhantbk@gmail.com>
2016-04-08 01:36:02 +1000

    Secod Initial

--- Medicine/FSredact.Designer.cs
+++ Medicine/FSredact.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.farmGroups = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.DVs = new System.Windows.Forms.ListBox();
-            this.knowledgeBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.knowledgeBindingSource)).BeginInit();
+            this.addFG = new System.Windows.Forms.Button();
+            this.addDV = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // farmGroups
             // 
-            this.farmGroups.DataSource = this.knowledgeBindingSource;
             this.farmGroups.FormattingEnabled = true;
             this.farmGroups.Location = new System.Drawing.Point(12, 25);
             this.farmGroups.Name = "farmGroups";
@@ -72,22 +71,48 @@
             this.DVs.Size = new System.Drawing.Size(223, 225);
             this.DVs.TabIndex = 3;
             // 
-            // knowledgeBindingSource
+            // addFG
             // 
-            this.knowledgeBindingSource.DataSource = typeof(Medicine.Knowledge);
+            this.addFG.Location = new System.Drawing.Point(12, 256);
+            this.addFG.Name = "addFG";
+            this.addFG.Size = new System.Drawing.Size(223, 36);
+            this.addFG.TabIndex = 4;
+            this.addFG.Text = "Добавить";
+            this.addFG.UseVisualStyleBackColor = true;
+            // 
+            // addDV
+            // 
+            this.addDV.Location = new System.Drawing.Point(274, 256);
+            this.addDV.Name = "addDV";
+            this.addDV.Size = new System.Drawing.Size(223, 36);
+            this.addDV.TabIndex = 5;
+            this.addDV.Text = "Добавить";
+            this.addDV.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(324, 313);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(173, 40);
+            this.exit.TabIndex = 6;
+            this.exit.Text = "Вернуться к выбору базы";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
             // FSredact
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 358);
+            this.ClientSize = new System.Drawing.Size(513, 367);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.addDV);
+            this.Controls.Add(this.addFG);
             this.Controls.Add(this.DVs);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.farmGroups);
             this.Name = "FSredact";
             this.Text = "Редактирование фармакологического справочника";
-            ((System.ComponentModel.ISupportInitialize)(this.knowledgeBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,6 +124,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox DVs;
-        private System.Windows.Forms.BindingSource knowledgeBindingSource;
+        private System.Windows.Forms.Button addFG;
+        private System.Windows.Forms.Button addDV;
+        private System.Windows.Forms.Button exit;
     }
 }
--- Medicine/FSredact.cs
+++ Medicine/FSredact.cs
@@ -16,5 +16,10 @@ namespace Medicine
         {
             InitializeComponent();
         }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
